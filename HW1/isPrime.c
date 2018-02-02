8e4fb7226d88b2f9f43ea3238e7e0eb820337c7d
Blake <bjsharp@vt.edu>
2018-02-01 23:57:55 -0500

    Figured out the isPrime method!

---
+++ HW1/isPrime.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+void main(){
+
+	// initialization
+	int N, count, upperBound;
+	
+	//text for screen, and reading input
+	printf("Enter a number:");
+	scanf("%d", &N);
+
+	count = 0; // used to determine if prime
+	upperBound = (int)sqrt(N)+1; // tripped up by the upper bound, but added 1 to be safe
+
+
+	//determines the amount of divisors
+	for (int i = 1; i <= upperBound; i++){
+		if(N%i == 0)
+			count++;
+	}		
+
+	//if only one divisor (1), the number is prime
+	if (count > 1)
+		printf("%d is not prime.\n", N);
+	else
+		printf("%d is prime.\n", N);
+
+}
